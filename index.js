61729620ab1b384fc0f5dba21998d511231a280d
mwhyus <mwahyus17@gmail.com>
2024-02-25 14:46:52 +0700

    handling product route

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ const url = require('url')
 
 // SERVER
 
-const replaceTemplate = (tempCard, items) => {
-  let output = tempCard.replace(/{%PRODUCTNAME%}/g, items.productName)
-  output = output.replace(/{%ID%}/g, items.id)
+const replaceTemplate = (temp, items) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, items.productName)
+  output = output.replace(/{%ID%}/g, (items.id - 1))
   output = output.replace(/{%IMAGE%}/g, items.image)
   output = output.replace(/{%PRICE%}/g, items.price)
   output = output.replace(/{%FROM%}/g, items.from)
@@ -55,19 +55,24 @@ const data = fs.readFileSync(`${__dirname}/text/listFruit.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true)
   const pathUrl = ['/', '/overview'];
 
-  if (pathUrl.includes(pathName)) {
+  if (pathUrl.includes(pathname)) {
     res.writeHead(200, {'Content-type': 'text/html'});
 
     const cardsHtml = dataObj.map(items => replaceTemplate(tempCard, items)).join('')
     const output = tempOverview.replace('{%PRODUCT_CARDS}', cardsHtml)
 
     res.end(output);
-  } else if (pathName === '/product') {
-    res.end('This is the product');
-  } else if (pathName === '/api') {
+  } else if (pathname === '/product') {
+    res.writeHead(200, {'Content-type': 'text/html'});
+
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product)
+
+    res.end(output);
+  } else if (pathname === '/api') {
     res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
   } else {
